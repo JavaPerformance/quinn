f18bc1f5b959b769d9d6ff2a3c819295348aa0ba
Paul Anderson <paul@paul-anderson.com>
2026-07-10 16:46:14 +0200

    proto: expose recovery telemetry in PathStats

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -1324,6 +1324,12 @@ impl Connection {
         stats.path.cwnd = self.path.congestion.window();
         stats.path.bandwidth_estimate = self.path.congestion.metrics().bandwidth_estimate;
         stats.path.current_mtu = self.path.mtud.current_mtu();
+        stats.path.bytes_in_flight = self.path.in_flight.bytes;
+        stats.path.packets_in_flight = self.path.in_flight.ack_eliciting;
+        stats.path.min_rtt = self.path.rtt.min();
+        stats.path.latest_rtt = self.path.rtt.latest();
+        stats.path.rtt_variance = self.path.rtt.variance();
+        stats.path.pto_count = self.pto_count;
 
         stats
     }
